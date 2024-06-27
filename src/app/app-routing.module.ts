3c96980f6b91071f0e3cd36e364c8e87556968aa
Salman Azeez <salmanazeez15@gmail.com>
2024-06-27 14:50:30 +0530

    Add project final files

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { CommonModule } from '@angular/common';
+
+
+const routes: Routes = [
+  {
+    path: "",
+    component: LandingPageComponent
+  },
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
